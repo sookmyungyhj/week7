5801aff8451bfd1d687bf05d05e3157156b50d4a
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-15 15:47:22 +0900

    2
    static

--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int flag = 1;
-	int y;
+	int i;
 	
-	while(flag != 0)
+	for (i=0;i<5;i++)
 	{
-		y = 3;
-		flag = 0;
+		int temp=1;
+		printf("temp = %d\n",temp);
+		temp++;
 	}
-	y=4;
 	
 	return 0;
 }
